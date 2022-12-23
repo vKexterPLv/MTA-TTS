1a61508a865b52244ce55e922f949898aa464ca1
vKexterPLv <120136795+vKexterPLv@users.noreply.github.com>
2022-12-23 06:13:16 +0100

    Create s.lua

---
+++ s.lua
@@ -0,0 +1,15 @@
+function TTS(plr,mes)
+	triggerClientEvent("playTTS",resourceRoot,plr,mes)
+end
+
+addCommandHandler("ttstoall",function(plr,cmd,...)
+	msg = table.concat({...}," ")
+	for k,v in pairs(getElementsByType("player")) do 
+		TTS(v,msg)
+	end
+end)
+
+addCommandHandler("ttstome",function(plr,cmd,...)
+	msg = table.concat({...}," ")
+	TTS(plr,msg)
+end)
